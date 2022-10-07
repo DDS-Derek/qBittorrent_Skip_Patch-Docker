9a9e80d861005b1a5c73489921b56014dd3db46d
DDSRem <1448139087@qq.com>
2022-10-07 19:55:11 +0800

    fix

--- test.sh
+++
@@ -1,9 +0,0 @@
-apt-get update -y && \
-apt-get install wget -y && \
-mkdir /build && \
-cd /build && \
-wget https://raw.githubusercontent.com/ChisBread/qbittorrent_skip_patch/main/qbittorrent-nox-static.sh && \
-bash /build/qbittorrent-nox-static.sh all -i && \
-qbitorrent_github_tag=release-4.3.9 \
-libtorrent_github_tag=LPE_v0.4 \
-bash qbittorrent-nox-static.sh qbittorrent
