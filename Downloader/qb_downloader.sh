0ab8df3ea21be0541faafcd6250859ed2fa1ca07
DDSRem <1448139087@qq.com>
2022-11-14 21:28:06 +0800

    fix

--- Downloader/qb_downloader.sh
+++ Downloader/qb_downloader.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
+mkdir -p /qb/4_4_x_RC_1_2
+mkdir -p /qb/4_3_x_RC_1_2
+mkdir -p /qb/4_2_x_RC_1_2
 
-wget https://github.com/Ghost-chu/qbittorrent-nox-static/releases/download/manual-4_4_x_RC_1_2-fasthash/aarch64-qbittorrent-nox
-wget https://github.com/Ghost-chu/qbittorrent-nox-static/releases/download/manual-4_4_x_RC_1_2-fasthash/aarch64-qt5-qbittorrent-nox
-wget https://github.com/Ghost-chu/qbittorrent-nox-static/releases/download/manual-4_4_x_RC_1_2-fasthash/armhf-qbittorrent-nox
-wget https://github.com/Ghost-chu/qbittorrent-nox-static/releases/download/manual-4_4_x_RC_1_2-fasthash/armhf-qt5-qbittorrent-nox
-wget https://github.com/Ghost-chu/qbittorrent-nox-static/releases/download/manual-4_4_x_RC_1_2-fasthash/armv7-qbittorrent-nox
-wget https://github.com/Ghost-chu/qbittorrent-nox-static/releases/download/manual-4_4_x_RC_1_2-fasthash/armv7-qt5-qbittorrent-nox
-wget https://github.com/Ghost-chu/qbittorrent-nox-static/releases/download/manual-4_4_x_RC_1_2-fasthash/x86_64-qbittorrent-nox
-wget https://github.com/Ghost-chu/qbittorrent-nox-static/releases/download/manual-4_4_x_RC_1_2-fasthash/x86_64-qt5-qbittorrent-nox
-
-
-wget https://github.com/Ghost-chu/qbittorrent-nox-static/releases/download/manual-4_3_x_RC_1_2-fasthash/aarch64-qt5-qbittorrent-nox
-wget https://github.com/Ghost-chu/qbittorrent-nox-static/releases/download/manual-4_3_x_RC_1_2-fasthash/armhf-qt5-qbittorrent-nox
-wget https://github.com/Ghost-chu/qbittorrent-nox-static/releases/download/manual-4_3_x_RC_1_2-fasthash/armv7-qt5-qbittorrent-nox
-wget https://github.com/Ghost-chu/qbittorrent-nox-static/releases/download/manual-4_3_x_RC_1_2-fasthash/x86_64-qt5-qbittorrent-nox
+wget -P /qb/4_4_x_RC_1_2 https://github.com/Ghost-chu/qbittorrent-nox-static/releases/download/manual-4_4_x_RC_1_2-fasthash/aarch64-qbittorrent-nox
+wget -P /qb/4_4_x_RC_1_2 https://github.com/Ghost-chu/qbittorrent-nox-static/releases/download/manual-4_4_x_RC_1_2-fasthash/aarch64-qt5-qbittorrent-nox
+wget -P /qb/4_4_x_RC_1_2 https://github.com/Ghost-chu/qbittorrent-nox-static/releases/download/manual-4_4_x_RC_1_2-fasthash/armhf-qbittorrent-nox
+wget -P /qb/4_4_x_RC_1_2 https://github.com/Ghost-chu/qbittorrent-nox-static/releases/download/manual-4_4_x_RC_1_2-fasthash/armhf-qt5-qbittorrent-nox
+wget -P /qb/4_4_x_RC_1_2 https://github.com/Ghost-chu/qbittorrent-nox-static/releases/download/manual-4_4_x_RC_1_2-fasthash/armv7-qbittorrent-nox
+wget -P /qb/4_4_x_RC_1_2 https://github.com/Ghost-chu/qbittorrent-nox-static/releases/download/manual-4_4_x_RC_1_2-fasthash/armv7-qt5-qbittorrent-nox
+wget -P /qb/4_4_x_RC_1_2 https://github.com/Ghost-chu/qbittorrent-nox-static/releases/download/manual-4_4_x_RC_1_2-fasthash/x86_64-qbittorrent-nox
+wget -P /qb/4_4_x_RC_1_2 https://github.com/Ghost-chu/qbittorrent-nox-static/releases/download/manual-4_4_x_RC_1_2-fasthash/x86_64-qt5-qbittorrent-nox
 
 
-wget https://github.com/Ghost-chu/qbittorrent-nox-static/releases/download/manual-4_2_x_RC_1_2-fasthash/aarch64-qt5-qbittorrent-nox
-wget https://github.com/Ghost-chu/qbittorrent-nox-static/releases/download/manual-4_2_x_RC_1_2-fasthash/armhf-qt5-qbittorrent-nox
-wget https://github.com/Ghost-chu/qbittorrent-nox-static/releases/download/manual-4_2_x_RC_1_2-fasthash/armv7-qt5-qbittorrent-nox
-wget https://github.com/Ghost-chu/qbittorrent-nox-static/releases/download/manual-4_2_x_RC_1_2-fasthash/x86_64-qt5-qbittorrent-nox
+wget -P /qb/4_3_x_RC_1_2 https://github.com/Ghost-chu/qbittorrent-nox-static/releases/download/manual-4_3_x_RC_1_2-fasthash/aarch64-qt5-qbittorrent-nox
+wget -P /qb/4_3_x_RC_1_2 https://github.com/Ghost-chu/qbittorrent-nox-static/releases/download/manual-4_3_x_RC_1_2-fasthash/armhf-qt5-qbittorrent-nox
+wget -P /qb/4_3_x_RC_1_2 https://github.com/Ghost-chu/qbittorrent-nox-static/releases/download/manual-4_3_x_RC_1_2-fasthash/armv7-qt5-qbittorrent-nox
+wget -P /qb/4_3_x_RC_1_2 https://github.com/Ghost-chu/qbittorrent-nox-static/releases/download/manual-4_3_x_RC_1_2-fasthash/x86_64-qt5-qbittorrent-nox
+
+
+wget -P /qb/4_2_x_RC_1_2 https://github.com/Ghost-chu/qbittorrent-nox-static/releases/download/manual-4_2_x_RC_1_2-fasthash/aarch64-qt5-qbittorrent-nox
+wget -P /qb/4_2_x_RC_1_2 https://github.com/Ghost-chu/qbittorrent-nox-static/releases/download/manual-4_2_x_RC_1_2-fasthash/armhf-qt5-qbittorrent-nox
+wget -P /qb/4_2_x_RC_1_2 https://github.com/Ghost-chu/qbittorrent-nox-static/releases/download/manual-4_2_x_RC_1_2-fasthash/armv7-qt5-qbittorrent-nox
+wget -P /qb/4_2_x_RC_1_2 https://github.com/Ghost-chu/qbittorrent-nox-static/releases/download/manual-4_2_x_RC_1_2-fasthash/x86_64-qt5-qbittorrent-nox
 
